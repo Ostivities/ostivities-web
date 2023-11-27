f2112d908d66b73502e888eda628eb74130555f4
dreezy305 <bankoleidris@gmail.com>
2023-11-27 07:51:57 +0100

    coming soon

--- app/components/sections/ComingSoon.tsx
+++ app/components/sections/ComingSoon.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Heading3, Paragraph } from "../typography/Typography";
 import Section from "./Section";
 
 function ComingSoon(): JSX.Element {
   return (
     <Section>
       <div className="mx-auto">
-        <div className="w-full coming-soon mx-auto">
-          <div className="flex flex-col text-left ml-auto space-y-3"></div>
+        <div className="w-full coming-soon mx-auto flex flex-row items-center">
+          <div className="flex flex-col text-left space-y-3 w-2/3 items-start pl-16">
+            <Heading3
+              content="Coming Soon"
+              className="w-3/4 text-left text-white"
+            />
+            <Paragraph
+              content="Stay tuned for an app that brings festivities to life and connects you with vibrant celebrations. Don't miss out — the countdown to cultural joy is on! 🎉🌟"
+              className="w-3/4 text-white text-lg text-left"
+            />
+          </div>
         </div>
       </div>
     </Section>
