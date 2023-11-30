334a8ebe7b72f1c151f5aecdd4aeb2200a2ac627
dreezy305 <bankoleidris@gmail.com>
2023-11-30 17:53:02 +0100

    footer responsiveness

--- app/components/sections/ComingSoon.tsx
+++ app/components/sections/ComingSoon.tsx
@@ -5,7 +5,7 @@ import Section from "./Section";
 function ComingSoon(): JSX.Element {
   return (
     <Section>
-      <div className="mx-auto">
+      <div className="mx-auto hidden md:hidden lg:block xl:block">
         <div className="w-full coming-soon mx-auto flex flex-row items-center">
           <div className="flex flex-col text-left space-y-3 w-2/3 items-start pl-16">
             <Heading3
