21b3a8245df5ac0d901610d73385a3bfed0a8203
dreezy305 <bankoleidris@gmail.com>
2023-12-02 11:20:12 +0100

    switch component

--- app/components/forms/ForgotPasswordForm.tsx
+++ app/components/forms/ForgotPasswordForm.tsx
@@ -1,11 +1,60 @@
 "use client";
 import { Heading3, Paragraph } from "@/app/components/typography/Typography";
+import { Button, Form, Input } from "antd";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 function ForgotPasswordForm(): JSX.Element {
   const router = useRouter();
-  return <div>ForgotPasswordForm</div>;
+  const [form] = Form.useForm();
+
+  const onFinish = (value: object) => {
+    console.log(value);
+  };
+  return (
+    <Form
+      name="validateOnly"
+      layout="vertical"
+      onFinish={onFinish}
+      autoComplete="off"
+      form={form}
+      className="w-full font-BricolageGrotesqueRegular flex flex-col"
+      style={{ fontFamily: "BricolageGrotesqueRegular" }}
+    >
+      <Form.Item
+        label="Email Address"
+        style={{ fontFamily: "BricolageGrotesqueRegular" }}
+        className="font-BricolageGrotesqueRegular"
+      >
+        <Form.Item
+          noStyle
+          rules={[{ required: true, message: "Please input your email" }]}
+        >
+          <Input
+            type="email"
+            placeholder="Enter your email"
+            className="placeholder:font-BricolageGrotesqueRegular"
+          />
+        </Form.Item>
+      </Form.Item>
+
+      <Form.Item>
+        <Button
+          type="primary"
+          htmlType="submit"
+          className="font-BricolageGrotesqueLight text-base"
+          style={{
+            background: "#E20000",
+            borderRadius: "20px",
+            width: "100%",
+            height: "51px",
+          }}
+        >
+          Continue
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
 
 export default ForgotPasswordForm;
