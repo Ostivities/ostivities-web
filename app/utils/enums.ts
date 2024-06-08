a2cc2675c4454efdf367cde0e0aacba71ec9a470
dreezy305 <bankoleidris@gmail.com>
2024-06-08 23:12:55 +0100

    api integration

--- app/utils/enums.ts
+++ app/utils/enums.ts
@@ -12,3 +12,14 @@ export enum TICKET_TYPE {
   PAID = "PAID",
   FREE = "FREE",
 }
+
+
+export enum HttpMethod {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
+  PATCH = "PATCH",
+  OPTIONS = "OPTIONS",
+  HEAD = "HEAD",
+}
