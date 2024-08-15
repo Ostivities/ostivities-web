32eac9c1946d4d82179c53fab132ade97859bc33
Ayatullah Olowu <108243609+Big-Ayat@users.noreply.github.com>
2024-08-15 18:49:40 +0100

    fix

--- app/components/sections/ComingSoon.tsx
+++ app/components/sections/ComingSoon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import comingsoon from "@/public/comingsoon.svg"; // Assuming this is the image you want to use
+import comingsoon from "@/public/feature.gif"; // Assuming this is the image you want to use
 
 function ComingSoon(): JSX.Element {
   return (
@@ -8,7 +8,7 @@ function ComingSoon(): JSX.Element {
       <div className="flex flex-row items-center space-x-3">
       </div>
       <div className="mx-auto hidden md:hidden lg:flex lg:justify-center lg:items-center xl:flex xl:justify-center xl:items-center w-full h-full">
-        <Image src={comingsoon} alt="hero" className="ms-1" width={1820} height={100} /> {/* Replace with actual image and dimensions */}
+        <Image src={comingsoon} alt="hero" className="ms-1" width={1820} height={100} unoptimized /> {/* Replace with actual image and dimensions */}
       </div>
     </div>
   );
