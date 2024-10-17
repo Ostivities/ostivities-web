5cb0bdce8e8b5c29f3f7e0f80442d9f1edc7723e
Ayatullah Olowu <108243609+Big-Ayat@users.noreply.github.com>
2024-10-17 12:46:27 +0100

    fix

--- app/components/sections/Faqs.tsx
+++ app/components/sections/Faqs.tsx
@@ -7,20 +7,21 @@ function Faqs(): JSX.Element {
   const onChange = (key: string | string[]) => {
     console.log(key);
   };
+
   return (
     <Section>
       <div
         className="flex flex-col space-y-5 md:w-4/5 lg:w-full xl:w-full md:mx-auto lg:mx-0 xl:mx-0 h-auto md:h-auto lg:h-[700px] xl:h-[700px]"
-        // style={{ height: "700px" }}
       >
-        <div className="flex flex-col w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3">
+        {/* Adjust this div to allow full width */}
+        <div className="flex flex-col w-full md:w-full lg:w-1/3 xl:w-1/3">
           <Small
             content="FAQ"
             className="text-OWANBE_PRY text-lg tracking-wider uppercase"
           />
           <Heading3
             content="Frequently asked questions"
-            className="w-full md:w-full lg:w-4/4 xl:w-3/4 capitalize"
+            className="w-full md:w-full capitalize"
           />
         </div>
         <Accordion />
