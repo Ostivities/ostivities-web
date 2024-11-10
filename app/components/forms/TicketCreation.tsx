52504194a0e2ed387f72e2234104f6c26b5591c7
Ostivities <ostivities@gmail.com>
2024-11-10 20:32:41 +0100

    dd

--- app/components/forms/TicketCreation.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function TicketCreation(): JSX.Element {
-  return <div>Single ticket</div>;
-}
-
-export default TicketCreation;
--- app/components/forms/events-created/Themes.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Themes(): JSX.Element {
-  return <div>THEME</div>;
-}
-
-export default Themes;
