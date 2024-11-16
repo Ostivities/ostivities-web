02e2fa1945726deadbc7283e9db7d3ace88a0a36
Ayatullah Olowu <108243609+Big-Ayat@users.noreply.github.com>
2024-11-16 23:53:15 +0100

    fix

--- app/components/AuthLayout/AuthLayout.tsx
+++ app/components/AuthLayout/AuthLayout.tsx
@@ -12,7 +12,7 @@ function AuthLayout({ children }: IAuthLayout): JSX.Element {
         <div className="md:container md:mx-auto px-5 pt-8 pb-12 lg:pb-0 md:pt-6 lg:pt-0">
           <div className="flex md:flex-row flex-col mt-4 md:gap-12 gap-16 relative md:items-center auth-background"> 
             <div className="w-5/12 flex flex-col space-y-0 pt-6">
-              <h5 className="leading-10 w-max font-BricolageGrotesqueRegular welcome-heading md:text-5xl text-4xl">
+              <h5 className="leading-10 w-max font-BricolageGrotesqueRegular welcome-heading md:text-4xl text-2xl">
                 Welcome to
               </h5>
               <Heading3
