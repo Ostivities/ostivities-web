c767ba476558fe2e3de841ccbf03f6527e057b6a
Ayatullah Olowu <108243609+Big-Ayat@users.noreply.github.com>
2024-11-18 09:18:22 +0100

    fixes

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -1,17 +1,14 @@
 "use client";
 import FeatureBg from "@/public/feature.svg";
-import { ArrowRightOutlined } from '@ant-design/icons';
-import Image from 'next/image';
-import React from 'react';
-import Section from './Section';
-import H3 from '@/app/ui/atoms/H3';
-import Link from 'next/link';
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
+import Section from "./Section";
+import Link from "next/link";
+import { motion } from "framer-motion";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { useState, useEffect } from 'react';
-import { motion } from "framer-motion";
-import '@/app/DynamicHeading.css';
+import "@/app/DynamicHeading.css";
 
 // Dynamic import of react-slick
 const Slider = dynamic(() => import("react-slick"), { ssr: false });
@@ -21,11 +18,25 @@ function Hero(): JSX.Element {
     dots: false,
     infinite: true,
     speed: 3200,
-    slidesToShow: 6,
+    slidesToShow: 7,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 0,
-    cssEase: 'linear',
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: { slidesToShow: 4, slidesToScroll: 1 },
+      },
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 2, slidesToScroll: 1 },
+      },
+      {
+        breakpoint: 480,
+        settings: { slidesToShow: 3, slidesToScroll: 1 },
+      },
+    ],
   };
 
   const slides = [
@@ -52,13 +63,12 @@ function Hero(): JSX.Element {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setAnimation("slide-out"); // Start exit animation
+      setAnimation("slide-out");
       setTimeout(() => {
-        setWordIndex((prevIndex) => (prevIndex + 1) % words.length); // Update word
-        setAnimation("slide-in"); // Start entry animation
-      }, 300); // Match duration of slide-out animation
-    }, 2000); // Change word every 2 seconds
-
+        setWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+        setAnimation("slide-in");
+      }, 300);
+    }, 2000);
     return () => clearInterval(interval);
   }, []);
 
@@ -69,6 +79,7 @@ function Hero(): JSX.Element {
 
   return (
     <Section>
+      {/* Hero Text Section */}
       <motion.div
         className="flex flex-col space-y-5 pt-5 mt-3 md:pt-8 md:mt-5 lg:pt-0 lg:mt-0 lg:space-y-0 lg:flex-row lg:space-x-8 lg:items-center"
         initial={{ opacity: 0 }}
@@ -77,22 +88,27 @@ function Hero(): JSX.Element {
       >
         <div className="flex flex-col items-center space-y-8 md:space-y-6 lg:items-start lg:w-1/2">
           <h3 className="text-center text-xl lg:text-3xl lg:text-left lg:w-[70%] font-semibold">
-            Connecting You to the <span className={`animated-word ${animation}`} style={{ color: currentColor }}>{currentWord}</span> That Matter Most.
+            Connecting You to the{" "}
+            <span
+              className={`animated-word ${animation}`}
+              style={{ color: currentColor }}
+            >
+              {currentWord}
+            </span>{" "}
+            That Matter Most.
           </h3>
           <motion.div
             className="flex flex-col space-y-4"
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 0.5 }} // Optional delay for smoother animation
+            transition={{ duration: 1, delay: 0.5 }}
           >
-
             <p className="font-BricolageGrotesqueMedium text-center lg:text-left lg:w-[87%] text-OWANBE_ASH text-lg">
-              Discover Events and Elevate Your Celebrations with Ostivities! Craft unforgettable
-              memories through seamless planning, ticket selling and event management. Start your journey with us today!
+              Discover Events and Elevate Your Celebrations with Ostivities!
+              Craft unforgettable memories through seamless planning, ticket
+              selling, and event management. Start your journey with us today!
             </p>
           </motion.div>
-
-
           <Link
             href="/discover"
             className="bg-OWANBE_SECONDARY hover:!bg-OWANBE_PRY transition-all duration-300 rounded-full hover:!text-white text-white px-8 py-3"
@@ -100,48 +116,38 @@ function Hero(): JSX.Element {
             <span className="pr-1">Explore Events</span>
           </Link>
         </div>
-        <br /><br /><br />
-
-        {/* Adding framer-motion to the image for a unique animation */}
+        <br />
         <motion.div
           className="w-full md:w-full lg:w-1/2 flex justify-center"
           initial={{ opacity: 0, y: 100 }}
-          animate={{ opacity: 2, y: 0 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 3 }}
         >
           <Image src={FeatureBg} alt="hero" className="w-full max-w-lg" />
         </motion.div>
       </motion.div>
-
+      
+      {/* Slider Section */}
       <motion.div
-        className="hidden lg:block lg:w-full pt-8 lg:pt-0"
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }}
-      >
-        <br /><br /><br />
-        {/* Wrap the Slider with motion.div for animation */}
-        <motion.div
-          className="flex flex-col space-y-4"
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ duration: 1, delay: 0.5 }} // Optional delay for smoother animation
-        >
-          <Slider {...settings}>
-            {slides.map((img, index) => (
-              <motion.div
-                key={index}
-                className="slide"
-                initial={{ opacity: 0, x: -50 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 0.5, delay: index * 0.2 }}
-              >
-                <img src={img} alt={`Slide ${index + 1}`} className="rounded-lg" />
-              </motion.div>
-            ))}
-          </Slider>
-        </motion.div>
-      </motion.div>
+      className="w-screen overflow-hidden pt-8 -mx-16" // Ensure full width and no margin
+      initial={{ x: "100%" }} // Start from the right side
+      animate={{ x: 0 }} // Move into the left side
+      transition={{ duration: 1.5, ease: "easeOut" }} // Slide-in animation
+    >
+      <Slider {...settings}>
+        {slides.map((img, index) => (
+          <motion.div
+            key={index}
+            className="slide"
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.2 }} // Staggered transition
+          >
+            <img src={img} alt={`Slide ${index + 1}`} className="w-full h-auto object-cover" />
+          </motion.div>
+        ))}
+      </Slider>
+    </motion.div>
     </Section>
   );
 }
---
+++ app/components/sections/Partners.tsx
@@ -0,0 +1,84 @@
+"use client";
+import FeatureBg from "@/public/feature.svg";
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+import Section from "./Section";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "@/app/DynamicHeading.css";
+
+// Dynamic import of react-slick
+const Slider = dynamic(() => import("react-slick"), { ssr: false });
+
+function Partners(): JSX.Element {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 3200,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: { slidesToShow: 4, slidesToScroll: 1 },
+      },
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 2, slidesToScroll: 1 },
+      },
+      {
+        breakpoint: 480,
+        settings: { slidesToShow: 2, slidesToScroll: 1 },
+      },
+    ],
+  };
+
+  const slides = [
+    "/partner1.svg",
+    "/partner2.svg",
+    "/partner3.svg",
+    "/partner4.svg",
+    "/partner5.svg",
+    "/partner1.svg",
+    "/partner4.svg",
+   "/partner2.svg",
+  ];
+
+
+  const [animation, setAnimation] = useState("slide-in");
+
+  return (
+    <Section>
+      {/* Slider Section */}
+      <motion.div
+     className="w-screen overflow-hidden pb-4 sm:pb-24 -mx-16"
+     // Ensure full width and no margin
+      initial={{ x: "100%" }} // Start from the right side
+      animate={{ x: 0 }} // Move into the left side
+      transition={{ duration: 1.5, ease: "easeOut" }} // Slide-in animation
+    >
+      <Slider {...settings}>
+        {slides.map((img, index) => (
+          <motion.div
+            key={index}
+            className="slide"
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.2 }} // Staggered transition
+          >
+            <img src={img} alt={`Slide ${index + 1}`} className="w-180px h-70px object-cover" />
+          </motion.div>
+        ))}
+      </Slider>
+    </motion.div>
+    </Section>
+  );
+}
+
+export default Partners;
